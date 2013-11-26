0030a440d667265b39c7ee84cb17082854a2ebd6
skillbilly <vanevery.eric@gmail.com>
2013-11-25 18:21:46 -0800

    Implemented voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,34 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  attr_accessible :body, :title, :image
+  has_many :votes, dependent: :destroy
+  attr_accessible :body, :title, :topic, :image
+  default_scope order('rank DESC')
+
   belongs_to :user
   belongs_to :topic 
+  after_create :create_vote
+
+
   mount_uploader :image, ImageUploader
+
+  def up_votes
+    self.votes.where(value: 1).count
+  end
+
+  def down_votes
+    self.votes.where(value: -1).count
+  end 
+
+ def points
+    self.votes.sum(:value).to_i
+  end  
+
+  def update_rank
+    age = (self.created_at - Time.new(1970,1,1)) / 86400
+    new_rank = points + age
+
+    self.update_attribute(:rank, new_rank)
+  end
 
   default_scope order('created_at DESC')
 
@@ -12,4 +37,10 @@ class Post < ActiveRecord::Base
   
   validates :topic, presence: true
   validates :user, presence: true  
+  private
+
+  # Who ever created a post, should automatically be set to "voting" it up.
+  def create_vote
+    self.user.votes.create(value: 1, post: self)
+  end  
 end
---
+++ app/models/vote.rb
@@ -0,0 +1,22 @@
+class Vote < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :post
+  attr_accessible :value, :post
+
+  validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }
+
+after_save :update_post
+
+  private
+
+  def update_post
+    self.post.update_rank  
+  end 
+   def update_rank
+    age = (self.created_at - Time.new(1970,1,1)) / 86400
+    new_rank = points + age
+
+    self.update_attribute(:rank, new_rank)
+  end
+
+end
